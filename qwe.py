6cd185c889ca90b4254ecbf2c6be5045a3fda856
koteyka5000 <romacat5000@gmail.com>
2022-02-07 09:32:19 +0300

    alpha version

--- qwe.py
+++ qwe.py
@@ -1,7 +1,21 @@
 import requests
 
+from bs4 import BeautifulSoup
+
 url = f'https://kidkodschool.github.io/welcome.html'
 response = requests.get(url)
 
 with open("./python_is_cool.html", 'wb') as f:
     f.write(response.content)
+
+query = "cats"
+url = f"https://www.kiddle.co/s.php?q={query}"
+page = requests.get(url).text
+soup = BeautifulSoup(page, "html.parser")
+for raw_img in soup.find_all("img"):
+    link = raw_img.get('src')
+    if link and link.startswith('https'):
+        response = requests.get(link)
+        with open("./today_avatar.jpg", 'wb') as f:
+            f.write(response.content)
+        break
