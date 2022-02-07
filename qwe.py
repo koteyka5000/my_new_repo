7a7f65043ac2d8ca957893ea3f1793e10b7605e9
koteyka5000 <romacat5000@gmail.com>
2022-02-07 09:53:37 +0300

    beta

--- qwe.py
+++ qwe.py
@@ -1,21 +1,24 @@
 import requests
+import sys
 
 from bs4 import BeautifulSoup
 
-url = f'https://kidkodschool.github.io/welcome.html'
-response = requests.get(url)
+# url = f'https://kidkodschool.github.io/welcome.html'
+# response = requests.get(url)
+#
+# with open("./python_is_cool.html", 'wb') as f:
+#     f.write(response.content)
 
-with open("./python_is_cool.html", 'wb') as f:
-    f.write(response.content)
-
-query = "cats"
+query = sys.argv[1] if len(sys.argv) > 1 else input('Введите тип вашего аватара')
 url = f"https://www.kiddle.co/s.php?q={query}"
 page = requests.get(url).text
 soup = BeautifulSoup(page, "html.parser")
-for raw_img in soup.find_all("img"):
+for raw_img in soup.find_all('img'):
     link = raw_img.get('src')
     if link and link.startswith('https'):
         response = requests.get(link)
         with open("./today_avatar.jpg", 'wb') as f:
             f.write(response.content)
         break
+else:
+    print('Аватар не найден...')
